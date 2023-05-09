28db6097a78cf4623619a4d78433ec1ecab65930
eldavina <eldaaavina@gmail.com>
2023-05-09 15:27:03 +0700

    last maybe2

--- src/component/molecules/User/UserDetailTipe/UserDetailTipe.molecules.jsx
+++ src/component/molecules/User/UserDetailTipe/UserDetailTipe.molecules.jsx
@@ -1,6 +1,6 @@
 import { useQuery } from "@apollo/client";
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import { getDetailTipesByUuidQuery } from "../../../../config/Apollo/lists";
 
 export const UserDetailTipe = () => {
